244aad88e4575d6e2cc1aad5eaa0f5865486e165
DESKTOP-OBJ2ITV\BABE WORRAWUT <worrawut.pitg@bumail.net>
2021-01-27 23:08:28 +0700

    Send Assignment

---
+++ Network Assignment Week 1/Assets/Codefind.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Codefind : MonoBehaviour
+{
+    public string FindWord;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void Names()
+    {
+
+        FindWord = inputField.GetComponent<Text>().text;
+
+        if (FindWord == "Doraemon")
+        {
+            textDisplay.GetComponent<Text>().text = "[" + FindWord + "]" + "is found";
+        }
+        else if (FindWord == "Nobita")
+        {
+            textDisplay.GetComponent<Text>().text = "[" + FindWord + "]" + "is found";
+        }
+        else if (FindWord == "Shizuka")
+        {
+            textDisplay.GetComponent<Text>().text = "[" + FindWord + "]" + "is found";
+        }
+        else if (FindWord == "Giant")
+        {
+            textDisplay.GetComponent<Text>().text = "[" + FindWord + "]" + "is found";
+        }
+        else if (FindWord == "Suneo")
+        {
+            textDisplay.GetComponent<Text>().text = "[" + FindWord + "]" + "is found";
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "[" + FindWord + "]" + "not found";
+        }
+
+
+    }
+}
